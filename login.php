b4c91db977930314f78a969aaa0c0be6d6399b14
boomsaka <boomsaka@udel.edu>
2020-08-17 00:04:45 -0400

    add logout and add login button in homepage

--- login.php
+++ login.php
@@ -1,4 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        body{ font: 14px sans-serif; }
+        .wrapper{ width: 350px; padding: 20px; }
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+        <h2>Login</h2>
+        <p>Please fill in your credentials to login.</p>
+        <form action="" method="post">
+            <div class="form-group <?php echo (!empty($customerId_err)) ? 'has-error' : ''; ?>">
+                <label>customerId</label>
+                <input type="text" name="customerId" class="form-control">
+                <!-- <span class="help-block"><?php echo $customerId_err; ?></span> -->
+            </div>    
+            <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+                <label>Password</label>
+                <input type="password" name="password" class="form-control">
+                <!-- <span class="help-block"><?php echo $password_err; ?></span> -->
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="Login">
+            </div>
+            <p>Don't have an account? <a href="register.php">Sign up now</a>.</p>
+        </form>
+    </div>    
+</body>
+</html>
+
 <?php
+error_reporting(0);
 //Include config file
 require_once "config.php";
 
@@ -36,42 +72,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 
 ?>
 
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Login</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
-    <style type="text/css">
-        body{ font: 14px sans-serif; }
-        .wrapper{ width: 350px; padding: 20px; }
-    </style>
-</head>
-<body>
-    <div class="wrapper">
-        <h2>Login</h2>
-        <p>Please fill in your credentials to login.</p>
-        <form action="" method="post">
-            <div class="form-group <?php echo (!empty($customerId_err)) ? 'has-error' : ''; ?>">
-                <label>customerId</label>
-                <input type="text" name="customerId" class="form-control">
-                <!-- <span class="help-block"><?php echo $customerId_err; ?></span> -->
-            </div>    
-            <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
-                <label>Password</label>
-                <input type="password" name="password" class="form-control">
-                <!-- <span class="help-block"><?php echo $password_err; ?></span> -->
-            </div>
-            <div class="form-group">
-                <input type="submit" class="btn btn-primary" value="Login">
-            </div>
-            <p>Don't have an account? <a href="register.php">Sign up now</a>.</p>
-        </form>
-    </div>    
-</body>
-</html>
 
 
 
